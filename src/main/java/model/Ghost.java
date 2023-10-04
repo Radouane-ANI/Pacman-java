0f1715775173c7a36db1af7b5a932ac062deb12f
Enzo <enzo.w782@icloud.com>
2023-10-04 15:22:41 +0200

    merge

--- src/main/java/model/Ghost.java
+++ src/main/java/model/Ghost.java
@@ -1,6 +1,9 @@
 package model;
 
+import  config.MazeConfig;
 import java.util.Random;
+
+import geometry.IntCoordinates;
 import geometry.RealCoordinates;
 
 public enum Ghost implements Critter {
@@ -8,6 +11,7 @@ public enum Ghost implements Critter {
     // TODO: implement a different AI for each ghost, according to the description in Wikipedia's page
     BLINKY, INKY, PINKY, CLYDE;
 
+    static final MazeConfig config = MazeConfig.makeExample1();
     private RealCoordinates pos;
     private Direction direction = Direction.NONE;
     @Override
@@ -44,6 +48,29 @@ public enum Ghost implements Critter {
                 case 2 -> ghost.direction = Direction.SOUTH;
                 case 3 -> ghost.direction = Direction.WEST;
             }
+        }
+    }
+
+    public static void updatePinkyPositions() { //déplacements de pinky
+        IntCoordinates posPacMan = config.getPacManPos();
+        IntCoordinates posPinky = config.getPinkyPos();
+        char zonePacMan = (
+            posPacMan.x() > config.getHeight() ? 
+            (posPacMan.y() > config.getWidth() ? 'a' : 'b') : 
+            (posPacMan.y() > config.getWidth() ? 'c': 'd')
+            );
+        char zonePinky = (
+            posPinky.x() > config.getHeight() ? 
+            (posPinky.y() > config.getWidth() ? 'a' : 'b') : 
+            (posPinky.y() > config.getWidth() ? 'c': 'd')
+            );
+        if (zonePacMan == zonePinky){
+            //1.0 Recuperer segment de pacman (le couloir dans lequel il est)
+            //1.1 Recuperer la direction
+            //1.2 Definir la prochaine intersection qu'il rencontrera (coord)
+            //1.3 Prendre le chemin le plus cours pour y aller
+        }else{
+            //2.0 Se diriger vers cette zone
         }
     }
 
