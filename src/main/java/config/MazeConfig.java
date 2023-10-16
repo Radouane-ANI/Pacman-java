bb2f32d269f0888829ce1d0b4ae72d331d35fd6b
jwd <jwd@penguin>
2023-10-17 00:48:17 +0200

    Mise à jour : Simplification enorme du code, Ajout d'un dictionnaire

--- src/main/java/config/MazeConfig.java
+++ src/main/java/config/MazeConfig.java
@@ -9,6 +9,10 @@ import static config.Cell.Content.ENERGIZER; // Ajout de la constante ENERGIZER
 import java.io.File ; // Ajout de la classe File
 import java.io.FileNotFoundException ; // Ajout de la classe FileNotFoundException pour gérer l'exception si le fichier n'extiste pas
 import java.util.Scanner ; // Ajout de la classe Scanner pour lire le fichier
+import java.util.HashMap ; // Ajout de la classe HashMap pour stocker les caractères du fichier
+import java.util.Map;
+
+import config.Cell.Content;
 
 public class MazeConfig {
     public MazeConfig(Cell[][] grid, IntCoordinates pacManPos, IntCoordinates blinkyPos, IntCoordinates pinkyPos,
@@ -26,7 +30,26 @@ public class MazeConfig {
 
     private final Cell[][] grid;
     private final IntCoordinates pacManPos, blinkyPos, pinkyPos, inkyPos, clydePos;
-
+    private static Map<Character, Cell> itemDictionary = new HashMap<>();
+    private static void iniDictionary(){
+        if(itemDictionary.size() != 0) return;
+        itemDictionary.put('A', open(ENERGIZER));
+        itemDictionary.put('B', closed(ENERGIZER));
+        itemDictionary.put('C', hPipe(ENERGIZER));
+        itemDictionary.put('D', vPipe(ENERGIZER));
+        itemDictionary.put('E', swVee(ENERGIZER));
+        itemDictionary.put('F', nwVee(ENERGIZER));
+        itemDictionary.put('G', neVee(ENERGIZER));
+        itemDictionary.put('H', seVee(ENERGIZER));
+        itemDictionary.put('I', nTee(ENERGIZER));
+        itemDictionary.put('J', eTee(ENERGIZER));
+        itemDictionary.put('K', sTee(ENERGIZER));
+        itemDictionary.put('L', wTee(ENERGIZER));
+        itemDictionary.put('M', nU(ENERGIZER));
+        itemDictionary.put('N', eU(ENERGIZER));
+        itemDictionary.put('O', sU(ENERGIZER));
+        itemDictionary.put('P', wU(ENERGIZER));
+    }
     public IntCoordinates getPacManPos() {
         return pacManPos;
     }
@@ -64,6 +87,7 @@ public class MazeConfig {
     // TODO: mazes should be loaded from a text file
     public static MazeConfig makeExample1() {
         File maze = new File("src/main/resources/maze.txt") ; // Création d'un objet File qui contient le fichier maze.txt
+        iniDictionary();
         try{
             Scanner scanner = new Scanner(maze) ; // Création d'un objet Scanner pour lire le fichier
             int height = scanner.nextInt() ; // Lecture de la hauteur du labyrinthe
@@ -75,284 +99,23 @@ public class MazeConfig {
                 String line = scanner.nextLine();
                 int ji = -1;
                 for (int j = 0; j < line.length(); j++) {
-                    ji++;
-                    switch (line.charAt(j)) {
-                        case 'A':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                               grid[i][ji] = open(ENERGIZER);
-                               j+= 1;
-                               break;
-                            }
-
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                grid[i][ji] = open(DOT);
-                                j+= 1;
-                                break;
-                            }
-
-                            else{
-                                grid[i][ji] = open(NOTHING);
-                                break;
-                            }
-                        case 'B':   
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                grid[i][ji] = closed(ENERGIZER);
-                                j+= 1;
-                            break;
-                            }
-                            
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                grid[i][ji] = closed(DOT);
-                                j+= 1;
-                                break;
-                            }
-                            
-                            else{
-                                grid[i][ji] = closed(NOTHING);
-                                break;
-                            }
-                        case 'C':   
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = hPipe(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = hPipe(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = hPipe(NOTHING);
-                                    break;
-                                }
-                        case 'D':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                     grid[i][ji] = vPipe(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = vPipe(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = vPipe(NOTHING);
-                                    break;
-                                }
-                        case 'E':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = swVee(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = swVee(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = swVee(NOTHING);
-                                    break;
-                                } 
-                        case 'F':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = nwVee(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = nwVee(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = nwVee(NOTHING);
-                                    break;
-                                }
-                        case 'G':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = neVee(ENERGIZER);
-                                j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                     grid[i][ji] = neVee(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = neVee(NOTHING);
-                                    break;
-                                }
-                        case 'H':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = seVee(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = seVee(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = seVee(NOTHING);
-                                    break;
-                                }
-                        case 'I':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = nTee(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = nTee(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = nTee(NOTHING);
-                                    break;
-                                }
-                        case 'J':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = eTee(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = eTee(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = eTee(NOTHING);
-                                    break;
-                                }
-                        case 'K':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = sTee(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = sTee(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = sTee(NOTHING);
-                                    break;
-                                }
-                        case 'L':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = wTee(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = wTee(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = wTee(NOTHING);
-                                    break;
-                                }
-                        case 'M':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = nU(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = nU(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = nU(NOTHING);
-                                    break;
-                                }
-                        case 'N':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = eU(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = eU(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = eU(NOTHING);
-                                    break;
-                                }
-                        case 'O':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = sU(ENERGIZER);
-                                j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = sU(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = sU(NOTHING);
-                                    break;
-                                }
-                        case 'P':
-                            if(j+1 < line.length() && line.charAt(j+1) == 'e'){
-                                    grid[i][ji] = wU(ENERGIZER);
-                                    j+= 1;
-                                break;
-                                }
-                                
-                            if(j+1 < line.length() && line.charAt(j+1) == 'd'){
-                                    grid[i][ji] = wU(DOT);
-                                    j+= 1;
-                                    break;
-                                }
-                                
-                            else{
-                                    grid[i][ji] = wU(NOTHING);
-                                    break;
-                                }
+                    ji++; // index de la colonne
+                    char currentChar = line.charAt(j); // caractère courant
+                    char nextChar = (j + 1 < line.length()) ? line.charAt(j + 1) : ' '; // caractère suivant
+            
+                    if (itemDictionary.containsKey(currentChar)) { // Si le caractère courant est présent dans le dictionnaire
+                        Cell currentItem = itemDictionary.get(currentChar); // On récupère la cellule correspondante
+                        Content nextItem = (nextChar == 'e') ? ENERGIZER : (nextChar == 'd') ? DOT : NOTHING; // On récupère le contenu de la cellule coreespondante au caractère suivant
+                        grid[i][ji] = currentItem.updateNextItemType(nextItem); // On met à jour le contenu de la cellule
+            
+                        if (nextChar == 'e' || nextChar == 'd') {
+                            j += 1;
                         }
                     }
-                    i++;
+                    
                 }
+                i++;
+            }
                 return new MazeConfig(grid,
                 new IntCoordinates(3, 0),
                 new IntCoordinates(0, 3),
