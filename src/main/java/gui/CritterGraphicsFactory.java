b7819f54d1f8354130e683278698e3f56df732c3
boucheka <anas.bouchekif@etu.u-paris.fr>
2023-10-17 00:53:18 +0200

    Modification du code pour que lorsque le pacman change de direction son sprite change aussi d'angle

--- src/main/java/gui/CritterGraphicsFactory.java
+++ src/main/java/gui/CritterGraphicsFactory.java
@@ -4,33 +4,59 @@ import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import model.Critter;
+import model.Direction;
 import model.Ghost;
+import model.MazeState;
 import model.PacMan;
-
 
 public final class CritterGraphicsFactory {
     private final double scale;
+    private final MazeState state;
 
-    public CritterGraphicsFactory(double scale) {
+    public CritterGraphicsFactory(double scale, MazeState state) {
         this.scale = scale;
+        this.state = state;
     }
 
     public GraphicsUpdater makeGraphics(Critter critter) {
         var size = 0.7;
-        var url = (critter instanceof PacMan) ? "pacman.png" :
-                switch ((Ghost) critter) {
-                    case BLINKY -> "ghost_blinky.png";
-                    case CLYDE -> "ghost_clyde.png";
-                    case INKY -> "ghost_inky.png";
-                    case PINKY -> "ghost_pinky.png";
-                };
+        var url = (critter instanceof PacMan) ? "pacman.png" : switch ((Ghost) critter) {
+            case BLINKY -> "ghost_blinky.png";
+            case CLYDE -> "ghost_clyde.png";
+            case INKY -> "ghost_inky.png";
+            case PINKY -> "ghost_pinky.png";
+        };
         var image = new ImageView(new Image(url, scale * size, scale * size, true, true));
         return new GraphicsUpdater() {
             @Override
             public void update() {
                 image.setTranslateX((critter.getPos().x() + (1 - size) / 2) * scale);
                 image.setTranslateY((critter.getPos().y() + (1 - size) / 2) * scale);
-                // Debug.out("sprite updated");
+                if (critter instanceof PacMan) {
+                    if (state.GetBoulbi()) {
+                        image.setRotate(0);
+                        image.setScaleX(1);
+                    } else {
+                        switch (critter.getDirection()) {
+                            case SOUTH:
+                                image.setRotate(90);
+                                image.setScaleX(1);
+                                break;
+                            case NORTH:
+                                image.setRotate(270);
+                                image.setScaleX(1);
+                                break;
+                            case EAST:
+                                image.setRotate(0);
+                                image.setScaleX(1);
+                                break;
+                            case WEST:
+                                image.setRotate(0);
+                                image.setScaleX(-1);
+                                break;
+                        }
+                    }
+                }
             }
 
             @Override
