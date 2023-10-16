8cec8a588e1b1a50519e02591918bd7f57233207
boucheka <anas.bouchekif@etu.u-paris.fr>
2023-10-17 01:06:42 +0200

    Ajout du maze et modification des entrés de crittergraphicsfactory pour pouvoir manipuler le pacman

--- src/main/java/gui/GameView.java
+++ src/main/java/gui/GameView.java
@@ -21,8 +21,10 @@ public class GameView {
     }
 
     /**
-     * @param maze  le "modèle" de cette vue (le labyrinthe et tout ce qui s'y trouve)
-     * @param root  le nœud racine dans la scène JavaFX dans lequel le jeu sera affiché
+     * @param maze  le "modèle" de cette vue (le labyrinthe et tout ce qui s'y
+     *              trouve)
+     * @param root  le nœud racine dans la scène JavaFX dans lequel le jeu sera
+     *              affiché
      * @param scale le nombre de pixels représentant une unité du labyrinthe
      */
     public GameView(MazeState maze, Pane root, double scale) {
@@ -32,10 +34,11 @@ public class GameView {
         root.setMinWidth(maze.getWidth() * scale);
         root.setMinHeight(maze.getHeight() * scale);
         root.setStyle("-fx-background-color: #000000");
-        var critterFactory = new CritterGraphicsFactory(scale);
+        var critterFactory = new CritterGraphicsFactory(scale, maze);
         var cellFactory = new CellGraphicsFactory(scale);
         graphicsUpdaters = new ArrayList<>();
-        for (var critter : maze.getCritters()) addGraphics(critterFactory.makeGraphics(critter));
+        for (var critter : maze.getCritters())
+            addGraphics(critterFactory.makeGraphics(critter));
         for (int x = 0; x < maze.getWidth(); x++)
             for (int y = 0; y < maze.getHeight(); y++)
                 addGraphics(cellFactory.makeGraphics(maze, new IntCoordinates(x, y)));
