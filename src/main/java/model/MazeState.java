c88327f1787ae55e3bdb211c6600236184f81ede
Enzo <enzo.w782@icloud.com>
2023-10-20 12:26:52 +0200

    modification sur la branche " enzo "

--- src/main/java/model/MazeState.java
+++ src/main/java/model/MazeState.java
@@ -52,6 +52,7 @@ public final class MazeState {
 
     public void update(long deltaTns) {
         Ghost.BLINKY.iaBlinky();
+        Ghost.PINKY.iaPinky();
         // FIXME: too many things in this method. Maybe some responsibilities can be
         // delegated to other methods or classes?
         for (var critter : critters) {
