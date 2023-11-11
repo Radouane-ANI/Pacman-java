1d61b45361794c81d82f91f797c26f39709a8e55
Marwane Benz <marwane.benrezzoug@etu.u-paris.fr>
2023-11-11 14:12:38 +0100

    Changement du style des boutons des ecrans de fins de jeu et d'accueil et amelioration dud code #30

--- src/main/java/gui/MainMenu.java
+++ src/main/java/gui/MainMenu.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import model.ButtonAction;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -24,12 +25,16 @@ public class MainMenu {
          // Création du bouton pour exit
         Button exitButton = new Button("Exit");
         exitButton.setOnAction(e -> System.exit(0));
+        exitButton.setStyle("-fx-background-color: black; -fx-text-fill: violet; -fx-font-size: 16px;");
+        applyHoverAnimation(exitButton);
         exitButton.setLayoutX(420);
         exitButton.setLayoutY(200);
         
         // Création du bouton pour jouer
         Button playButton = new Button("Jouer");
         playButton.setOnAction(e -> startGame());
+        playButton.setStyle("-fx-background-color: black; -fx-text-fill: violet; -fx-font-size: 16px;");
+        applyHoverAnimation(playButton);
         playButton.setLayoutX(40);
         playButton.setLayoutY(200);
 
@@ -50,6 +55,11 @@ public class MainMenu {
         Game game = new Game(stage);
         // Définition de la scène de jeu comme scène principale de la fenêtre
         stage.setScene(game.getScene());
+    }
+
+    private void applyHoverAnimation(Button button) {
+        button.setOnMousePressed(e -> button.setStyle("-fx-background-color: violet; -fx-text-fill: black; -fx-font-size: 16px;"));
+        button.setOnMouseReleased(e -> button.setStyle("-fx-background-color: black; -fx-text-fill: violet; -fx-font-size: 16px;"));
     }
 
     public Scene getScene() {
--- src/main/java/model/FinalScreen.java
+++ src/main/java/model/FinalScreen.java
@@ -1,6 +1,7 @@
 package model;
 
 import gui.MainMenu;
+import javafx.animation.ScaleTransition;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,6 +11,7 @@ import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class FinalScreen {
@@ -19,24 +21,20 @@ public class FinalScreen {
     private final Button homeButton = new Button("Home");
     boolean win;
 
-    public FinalScreen(ButtonAction playAgainAction, ButtonAction exitAction, Stage primaryStage, boolean win) {
+    public FinalScreen(ButtonAction playAgainAction, Stage primaryStage, boolean win) {
         this.win = win;
-        playAgainButton.setOnAction(e -> playAgainAction.performAction());
-        exitButton.setOnAction(e -> exitAction.performAction());
-        homeButton.setOnAction(e -> {
+
+        setupButton(playAgainButton, playAgainAction);
+        setupButton(exitButton, () -> {System.exit(0);});
+        setupButton(homeButton, () -> {
             MainMenu mainMenu = new MainMenu(primaryStage);
             primaryStage.setScene(mainMenu.getScene());
             primaryStage.show();
         });
-        String img;
-        if(win){
-            img="win.png";
-        }
-        else{
-            img="game_over.png";
-        }
-        
+
+        String img = win ? "win.png" : "game_over.png";
         var image = new ImageView(new Image(/* ../ressources/*/img, 500, 500, true, true));
+
         // Positionnez les boutons et l'image à l'emplacement souhaité
         playAgainButton.setLayoutX(200);
         playAgainButton.setLayoutY(500);
@@ -47,10 +45,19 @@ public class FinalScreen {
         image.setLayoutX(50);
         image.setLayoutY(50);
 
-        
-
         // Ajoutez les boutons et l'image au conteneur
         FinalScreenLayout.getChildren().addAll(playAgainButton, exitButton, homeButton, image);
+    }
+
+    private void setupButton(Button button, ButtonAction action) {
+        button.setOnAction(e -> action.performAction());
+        button.setStyle("-fx-background-color: black; -fx-text-fill: violet; -fx-font-size: 16px;");
+        applyHoverAnimation(button);
+    }
+
+    private void applyHoverAnimation(Button button) {
+        button.setOnMousePressed(e -> button.setStyle("-fx-background-color: violet; -fx-text-fill: black; -fx-font-size: 16px;"));
+        button.setOnMouseReleased(e -> button.setStyle("-fx-background-color: black; -fx-text-fill: violet; -fx-font-size: 16px;"));
     }
 
     Node getFinalScreenLayout(){return FinalScreenLayout;}
