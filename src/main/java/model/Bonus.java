b9c6ff3fb34a30c63936e4a8d8efbc46678fab53
Radouane Aouini <radouaneloo.aouini@gmail.com>
2023-11-16 10:52:46 +0100

    #11 creation d'une class pour les bonus et de leurs methodes communes

---
+++ src/main/java/model/Bonus.java
@@ -0,0 +1,79 @@
+package model;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import geometry.IntCoordinates;
+
+public enum Bonus {
+
+    CERISE, ECLAIR, COEUR;
+
+    private boolean apparut;
+    private boolean actif;
+    private IntCoordinates pos;
+
+    public IntCoordinates getPos() {
+        return pos;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public boolean isApparut() {
+        return apparut;
+    }
+
+    private static long time = System.currentTimeMillis();
+    private static Random random = new Random();
+
+    public static Bonus generateRandomBonus() {
+        // Générez un nombre aléatoire pour décider si un bonus doit apparaître
+        int rd = random.nextInt(3);
+
+        if (rd == 1) { // 1 chance sur 3 qu'un bonus apparait
+            // Générez un nombre aléatoire pour choisir le type de bonus
+            Bonus bonus = chooseRandomBonus();
+
+            // Vérifiez si le bonus n'est pas déjà apparu et n'est pas actif
+            if (!bonus.apparut) {
+                bonus.apparut = true; // Marquez le bonus comme apparu
+                return bonus;
+            }
+        }
+
+        return null; // Aucun bonus n'apparaît ou tous les bonus sont déjà apparus
+    }
+
+    private static Bonus chooseRandomBonus() {
+        // Générez un nombre aléatoire pour choisir le type de bonus
+        Bonus[] Bonuss = Bonus.values();
+        int randomIndex = random.nextInt(Bonuss.length);
+        return Bonuss[randomIndex];
+    }
+
+    public void spawnBonus() {
+        if (System.currentTimeMillis() - time > 3000) {
+            time = System.currentTimeMillis();
+            Bonus b = generateRandomBonus();
+            if (b != null) {
+                b.actif = true;
+                b.pos = new IntCoordinates(0, 0);
+
+                // Planifiez une tâche pour désactiver le bonus après 10 secondes
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+
+                    @Override
+                    public void run() {
+                        b.actif = false;
+                    }
+
+                }, 10000); // 10 000 millisecondes = 10 secondes
+
+            }
+        }
+    }
+}
