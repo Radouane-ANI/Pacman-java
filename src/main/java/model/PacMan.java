07abd0dbe5f514034e736a3b8054a667c10ad100
boucheki <jawad.bouchekif@etu.u-paris.fr>
2023-11-17 21:04:48 +0100

    Ajout de fonction pour les mouvements de pacman #29

--- src/main/java/model/PacMan.java
+++ src/main/java/model/PacMan.java
@@ -1,15 +1,19 @@
 package model;
 
+import static model.Ghost.config;
+import config.MazeConfig;
+import geometry.IntCoordinates;
 import geometry.RealCoordinates;
-
+import model.MazeState;
 /**
  * Implements Pac-Man character using singleton pattern. FIXME: check whether singleton is really a good idea.
  */
 public final class PacMan implements Critter {
+    
     private Direction direction = Direction.NONE;
     private RealCoordinates pos;
     private boolean energized;
-
+    private RealCoordinates prevpos;
     private PacMan() {
     }
 
@@ -32,14 +36,38 @@ public final class PacMan implements Critter {
 
     @Override
     public void setDirection(Direction direction) {
+        this.prevpos = this.pos;
         this.direction = direction;
     }
 
     @Override
     public void setPos(RealCoordinates pos) {
+        this.prevpos = this.pos;
         this.pos = pos;
     }
-
+    /**
+     * Fonction qui renvoie un bool si il y'a un mur en face de l'instance du PACMAN
+     * @param direction direction du pacman
+     * @return un bool si il y'a un mur en face de l'instance du PACMAN
+     */
+    public static boolean  estPossible(Direction direction){
+        var pacPos = PacMan.INSTANCE.getPos().round();
+        switch(direction){
+            case NORTH:
+                
+                return !config.getCell(pacPos).northWall();
+            case SOUTH:
+                
+                return !config.getCell(pacPos).southWall();
+            case EAST:
+                
+                return !config.getCell(pacPos).eastWall();
+            case WEST:
+               
+                return !config.getCell(pacPos).westWall();
+            default: return false;
+        }
+    }
     /**
      *
      * @return whether Pac-Man just ate an energizer
